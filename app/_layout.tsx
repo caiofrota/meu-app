eeb4abe969bf41ee03412e7cec02d3ba14eb8e10
Caio Frota <caiofrota@gmail.com>
2026-05-26 12:53:10 -0300

    Adiciona telas "Minhas Tarefas" e "Sobre" ao layout do Drawer

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
 import { Drawer } from "expo-router/drawer";
 
 export default function RootLayout() {
-  return <Drawer />;
+  return (
+    <Drawer>
+      <Drawer.Screen name="index" options={{ title: "Tarefas", drawerLabel: "Minhas Tarefas" }} />
+      <Drawer.Screen name="sobre" options={{ title: "Sobre", drawerLabel: "Sobre" }} />
+    </Drawer>
+  );
 }
